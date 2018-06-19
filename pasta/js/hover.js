8f8608ac23d4164556b8403888d7e3e2eb97a2b9
klausfontenelle <40373602+klausfontenelle@users.noreply.github.com>
2018-06-19 09:34:31 -0300

    Delete hover.js

--- pasta/js/hover.js
+++
@@ -1,8 +0,0 @@
-$().ready(function(){
-	$(".column img").mouseenter(function(){
-         $(this).css("opacity", "0.6")
-    })
-    $(".column img").mouseleave(function(){
-        $(this).css("opacity", "1")
-    });
-});
