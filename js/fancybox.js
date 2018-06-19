556d0f964d3bcc41f8b594c31f717acfc6d2c590
klausfontenelle <40373602+klausfontenelle@users.noreply.github.com>
2018-06-19 09:33:08 -0300

    Add files via upload

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,11 +1,3 @@
 $().ready(function(){
 	$(".lightbox").fancybox();
-
-	// $('a[href^="#"]').click(function(){
-	// $("html, body").animate({
-	// 	scrollTop: $( $.attr(this, "href")).offset().top
-	// }, 600);
-
-	// return false;
-	// })
 });
--- js/slide.js
+++ js/slide.js
@@ -1,33 +1,24 @@
 $(document).ready(function(){
 
-/*---------------------- HOME -----------------------*/
-/*---------------------- SUMIDO -----------------------*/
   $('#home')
     .hide()
     
-/*---------------------- APARECE -----------------------*/
   $('#home')
     .slideDown(1500)
 
-/*---------------------- SE CLICAR NO MENU -----------------------*/
   $('#item1')
     .click(function(){
       $('#home').slideUp("slow")
       $('#home').slideDown("slow")
   })
 
-
-
-/*---------------------- PORTFOLIO -----------------------*/
-/*---------------------- SUMIDO -----------------------*/
   $('#portifolio')
     .hide()
 
-/*---------------------- APARECE -----------------------*/
+
   $('#portifolio')
     .slideDown(1500)
 
-// ---------------------- SE CLICAR NO MENU -----------------------
   $('#item2')
     .click(function(){
       $('#portifolio').slideUp("slow")
